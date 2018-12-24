f0876716fa4b92d6076b7f27e8a0b037136c54d4
Suykum <jildiz852@gmail.com>
2018-12-24 15:07:52 +0300

    Перенести все виды из предыдущего задания на JSP[#75025]

--- chapter_004/src/main/java/ru/job4j/servlet2/UserCreateServlet.java
+++ chapter_004/src/main/java/ru/job4j/servlet2/UserCreateServlet.java
@@ -1,53 +1,39 @@
 package ru.job4j.servlet2;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+
 
 public class UserCreateServlet extends HttpServlet {
     private ValidateUser validateUserStore = ValidateUser.getValidateUserObject();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.append("<!DOCType html>"
-                + "<html>"
-                + "<head>"
-                + "<title>CREATE NEW USER</title>"
-                + "</head>"
-                + "<body>"
-                + "<h2>CREATE NEW USER</h2>"
-                + "<form method = 'post'>"
-                + "Name:<input type='text' name='name'><br>"
-                + "Login:<input type='text' name='login'><br>"
-                + "Email:<input type='text' name='email'><br>"
-                + "<input type='submit' value='CREATE'>"
-                + "</form>"
-                + "</body>"
-                + "</html>");
+        //resp.sendRedirect(String.format("%s/UserCreate.jsp", req.getContextPath()));
+        req.getRequestDispatcher(String.format("%s/UserCreate.jsp", req.getContextPath())).forward(req, resp);
 
-        writer.flush();
-        writer.close();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //resp.setContentType("text/html");
-        //PrintWriter writer = new PrintWriter(resp.getOutputStream());
+
         validateUserStore.createTable();
         String name = req.getParameter("name");
         String login = req.getParameter("login");
         String email = req.getParameter("email");
-        //writer.append(validateUserStore.add(new User(name, login, email)));
-        //writer.append("<br>");
-        //writer.append("<a href='UsersServlet.do'>Back to UsersServlet</a>");
-        //writer.append("<a href='Users.jsp'>Back to UsersJSP</a>");
-        //writer.flush();
-        //writer.close();
-        validateUserStore.add(new User(name, login, email));
-        resp.sendRedirect(String.format("%s/Users.jsp", req.getContextPath()));
+        String add = validateUserStore.add(new User(name, login, email));
+        if (!add.contains("Cannot")) {
+            resp.sendRedirect(String.format("%s/Users.jsp", req.getContextPath()));
+            //req.getRequestDispatcher(String.format("%s/Users.jsp", req.getContextPath())).forward(req, resp);
+        } else {
+            req.setAttribute("error", "Cannot be added");
+            doGet(req, resp);
+
+        }
+
     }
 }
--- chapter_004/src/main/webapp/UserCreate.jsp
+++ chapter_004/src/main/webapp/UserCreate.jsp
@@ -9,6 +9,12 @@
             Login:<input type='text' name='login'><br>
             Email:<input type='text' name='email'><br>
             <input type='submit' value='CREATE'>
+            <br>
+            <br>
+            <%String msg= (String) request.getAttribute("error");%>
+            <%if (msg !=null) {%>
+                <%=msg%>
+            <%}%>
         </form>
     </body>
 </html>
