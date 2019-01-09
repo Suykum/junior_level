d6222d243ec49dd46a640397d5d1067db8f9effd
Suykum <jildiz852@gmail.com>
2019-01-09 12:50:24 +0300

    Заполнить динамически таблицу.[#75030]

--- chapter_004/src/main/webapp/scripts/scripts.js
+++ chapter_004/src/main/webapp/scripts/scripts.js
@@ -15,3 +15,23 @@ function validate() {
     }
     return result;
 }
+
+function addRow() {
+    if (validate()) {
+        var name = document.getElementById("name").value;
+        var lastName = document.getElementById("lastname").value;
+        var genderObj = document.getElementsByName("gender");
+        var description = document.getElementById("description").value;
+        var gender;
+        if (genderObj[0].checked) {
+            gender = "male";
+        } else if (genderObj[1].checked) {
+            gender = "female";
+        }
+        $('#table tr:last').after('<tr><td>' + name + '</td><td>' + lastName + '</td><td>' + gender + '</td><td>' + description  +'</td></tr>')
+
+        document.getElementById("myForm").reset();
+    }
+}
+
+
